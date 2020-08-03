32560bee5471f2ba678c9bff5c035c26fef325d9
dmattia <david@transcend.io>
2020-08-03 10:58:42 -0500

    Added output with which policy ARNs are applied

--- outputs.tf
+++ outputs.tf
@@ -27,3 +27,8 @@ output "role_arn" {
   value       = module.service.role_arn
   description = "Arn of the task execution role"
 }
+
+output "policy_arns" {
+  value = module.service.policy_arns
+  description = "Amazon resource names of all policies set on the IAM Role execution task"
+}
