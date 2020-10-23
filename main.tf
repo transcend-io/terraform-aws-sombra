e37aa52c5a6dc07e5fe4f1eca5eae09cb7c33032
dmattia <david@transcend.io>
2020-10-23 16:40:42 -0500

    ahhhhhh

--- main.tf
+++ main.tf
@@ -138,16 +138,16 @@ module service {
   alb_security_group_ids = module.load_balancer.security_group_ids
   container_definitions = format(
     "[%s]",
-    join(",", distinct(concat(
-      # DO NOT SUBMIT: Just for testing
-      [var.extra_container_definitions[0]]
-      [module.container_definition.json_map],
-      [var.extra_container_definitions[1]]
-      [var.extra_container_definitions[2]]
-
-      # [module.container_definition.json_map],
-      # var.extra_container_definitions
-    )))
+    join(",", [
+      var.extra_container_definitions[0]
+      module.container_definition.json_map,
+      var.extra_container_definitions[1]
+      var.extra_container_definitions[2]
+    ])
+    # join(",", distinct(concat(
+    #   [module.container_definition.json_map],
+    #   var.extra_container_definitions
+    # )))
   )
 
   additional_task_policy_arns = concat(
