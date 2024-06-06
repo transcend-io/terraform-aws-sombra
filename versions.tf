b60b76e91749ec5043b979a554619ab4516dcf46
Dipack <dipack@transcend.io>
2024-06-06 15:01:20 -0700

    Modify IAM permissions block to allow for KMS GenerateRandom.
    Signed-off-by: Dipack <dipack@transcend.io>

--- versions.tf
+++ versions.tf
@@ -6,7 +6,7 @@ terraform {
     # We cannot use versions 3.16.0 or higher due to this regression:
     # https://github.com/hashicorp/terraform-provider-vault/issues/1907
     vault = {
-      source = "hashicorp/vault"
+      source  = "hashicorp/vault"
       version = "< 3.16.0"
     }
   }
