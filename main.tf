ae6b7a2d5fd27e6b6127b1e6c53808699add0339
David Mattia <david@transcend.io>
2025-01-23 22:13:02 +0000

    Add ssm support

--- main.tf
+++ main.tf
@@ -295,6 +295,11 @@ resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
   name = "${var.deploy_env}-${var.project_id}-ecs-instance-profile"
   role = aws_iam_role.ecs_instance_role.name
