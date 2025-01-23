908af522fb35a18574517dc2cbb6a50badf98f87
David Mattia <david@transcend.io>
2025-01-23 20:51:03 +0000

    update instance profile for lc

--- main.tf
+++ main.tf
@@ -272,10 +272,38 @@ resource "aws_launch_configuration" "llm_classifier_lc" {
   name          = "${var.deploy_env}-${var.project_id}-llm-classifier-lc"
   image_id      = data.aws_ami.amazon_linux_2.id
   instance_type = var.llm_classifier_instance_type
+  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "${var.deploy_env}-${var.project_id}-ecs-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "${var.deploy_env}-${var.project_id}-ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
 }
 
 locals {
