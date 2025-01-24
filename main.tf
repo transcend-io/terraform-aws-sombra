078706e1d9c7d606fa29b925f91b3bb59fba618d
David Mattia <david@transcend.io>
2025-01-24 15:44:39 +0000

    add anotha policy

--- main.tf
+++ main.tf
@@ -351,6 +351,11 @@ resource "aws_iam_role_policy_attachment" "ssm_instance_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
   name = "${var.deploy_env}-${var.project_id}-ecs-instance-profile"
   role = aws_iam_role.ecs_instance_role.name
